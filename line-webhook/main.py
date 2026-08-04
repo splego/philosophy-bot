c9e5fbf8d90100bd7fc3b1677a85c5782ed0e9bb
splego <info@splego.com>
2026-08-04 19:43:29 +0900

    webhook update

--- line-webhook/main.py
+++ line-webhook/main.py
@@ -1,24 +1,56 @@
 import json
 import boto3
 import uuid
+import urllib.request
 
 dynamodb = boto3.resource("dynamodb")
 table = dynamodb.Table("users")
 
 def lambda_handler(event, context):
-    print("EVENT:", json.dumps(event))
 
-    # とりあえずダミーデータ
-    user_id = str(uuid.uuid4())
+    body = json.loads(event["body"])
+    events = body.get("events", [])
 
-    # DB保存（あとで実装）
-    table.put_item(
-        Item = {
-            "userId": user_id,
-            "test": "GOOD!"
-        }
-    )
+    for e in events:
+        user_id = e["source"]["userId"]
+        event_type = e["type"]
+
+        table.put_item(Item={
+        "userId": user_id
+        })
+
+        if "replyToken" in e:
+            reply_token = e["replyToken"]
+            reply_message(reply_token, "登録さんきゅ～！")
+
     return {
         "statusCode": 200,
-        "body": json.dumps({"message": "written"})
+        "body": "OK"
     }
+
+def reply_message(reply_token, text):
+    url = "https://api.line.me/v2/bot/message/reply"
+
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {LINE_CHANNEL_ACCESS_TOKEN}"
+        }
+
+        data = {
+            "replyToken": reply_token,
+            "messages": [
+                {
+                    "type": "text",
+                    "text": text
+                }
+            ]
+       }
+
+    req = urllib.request.Request(
+        url,
+        data=json.dumps(data).encode("utf-8"),
+        headers=headers,
+        method="POST"
+    )
+
+    urllib.request.urlopen(req)
